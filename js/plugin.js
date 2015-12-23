645e5334114d95954a7874783b29c9a42f3f8e7f
qwant50 <qwantonline@gmail.com>
2015-12-23 15:45:14 +0200

    Plugin completed (v1.0)

--- js/plugin.js
+++ js/plugin.js
@@ -6,53 +6,70 @@
  */
 (function ($) {
 
+    // актуальные настройки, глобальные
+
+    var defaults = {times: 5, distance: 100, speed: 50};
+
     var methods = {
-        init : function( options ) {
+        init: function ( params) {
             // А ВОТ ЭТОТ
-        },
-        start : function( ) {
-            // ПОДХОД
-            $(this).effect("shake", {
-                times: 5,
-                distance: 100
-            }, 30);
+            var options = $.extend({}, defaults, params);
+
+            // инициализируем один раз
+            var init = $(this).data('myEffect');
+
+            if (init) {
+                return this;
+            } else {
+                $(this).data('myEffect', true);
+                return this.bind("mouseenter.myEffect",function(){
+                    // { "shake", {times, distance, directions}, speed}
+                    $(this).effect("shake", { 'times':options.times, 'distance':options.distance }, options.speed);
+
+                });
+            }
 
         },
-        stop : function( ) {
-            // ПРАВИЛЬНЫЙ
+        start: function (options) {
+
+
         },
-        update : function( content ) {
-            // !!!
+        effect: function ( params ) {
+            // ПРАВИЛЬНЫЙ
+            var options = $.extend({}, defaults, params);
+            return $(this).effect("shake", { 'times':options.times, 'distance':options.distance }, options.speed);
+        },
+        destroy: function () {
+            // возвращаем все назад
+            $(this).removeData('myEffect');
+            return $(this).unbind(".myEffect");
         }
     };
 
-    $.fn.myEffect = function ( method ) {
+    $.fn.myEffect = function (method) {
 
-        var defaults = { times: 5 , distance: 100 };
-        // актуальные настройки, глобальные
-        var options;
-
-        return this.each(function() {
-            options = $.extend({}, defaults, options, method);
-
-            // логика вызова метода
-            if ( methods[method] ) {
-                return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-            } else if ( typeof method === 'object' || ! method ) {
-                return methods.init.apply( this, arguments );
-            } else {
-                $.error( 'Метод с именем ' +  method + ' не существует для jQuery.tooltip' );
-            }
-
-
-        });
-
+        // логика вызова метода
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Метод с именем ' + method + ' не существует для jQuery.myEffect');
+        }
 
     };
 
-    $('button').click(function(){
-       $(this).myEffect('start').myEffect('start').myEffect('start').myEffect('start');
+    $('#init').on('click',function(){
+        $('td').myEffect({times: 10, distance: 20});
+        console.log(this);
     });
+    $('#destroy').on('click',function(){
+        $('td').myEffect('destroy');
+    });
+    $('#effect').on('click',function(){
+        $('td').myEffect('effect').myEffect('effect').myEffect('effect');
+    })
+
 
 
 })(jQuery);
